da47b6e76bfc8f11407ba4a05f965f3c6362a990
Andrés David Álvarez Tórrez <ndralvz@gmail.com>
2023-09-12 19:55:05 -0300

    feat(cards): :rocket::art: creates cards for dishes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { Home } from "./pages"
 
 const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <div>Hello world!</div>,
+		element: <Home />,
 	},
 ])
 
